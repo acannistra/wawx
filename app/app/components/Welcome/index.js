9e36f80207cd74b104170abff499b1995a1bef73
Tony Cannistra <tony.cannistra@gmail.com>
2018-01-18 16:36:20 -0800

    added disclaimer`

--- app/app/components/Welcome/index.js
+++ app/app/components/Welcome/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components'
 import {Row, Col} from 'elemental';
+import {Alert} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-tabs/style/react-tabs.css';
 
@@ -19,7 +20,7 @@ export default class Welcome extends React.Component{
 			<h1> NWAC v2 <small> by <a href="http://www.anthonycannistra.com/about">Tony Cannistra</a></small></h1>
 			<p>Click a region on the map. </p>
 
-			<div style={{'display' : 'inline-block'}} class="align-bottom">USE AT YOUR OWN RISK.</div>
+			<Alert color='danger'>USE AT YOUR OWN RISK. Active development ongoing. Information is possibly incorrect. Always consult with <a href="http://nwac.us">NWAC.us</a> before going outside. Leave feedback <a href="https://www.github.com/acannistra/wawx/issues/new">here</a>.</Alert>
 			<div>
 				<small><a target="_blank" href={"https://github.com/acannistra/wawx/tree/" + CODE_VERSION}>
 						 v.{CODE_VERSION}</a></small>
